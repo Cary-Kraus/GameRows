625ff07651a6c753fa9fb450daf327600d5cd35f
kraus <kraus@GESICHTSLOS>
2021-10-24 03:33:14 +0300

    Добавьте файлы проекта.

---
+++ GameRows/Form1.cs
@@ -0,0 +1,325 @@
+﻿using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Media;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace GameRows
+{
+    public partial class Form1 : Form
+    {
+        Button[,] buttons;
+        Image[] images;
+
+        Rows rows;
+
+        const string filename = ".png";
+
+        int tk;
+        int i;
+        string c;
+        int record = int.Parse(File.ReadAllText(Rows.filename));
+        SoundPlayer sound;
+
+        void Start()
+        {
+            rows = new Rows(ShowBox, PlaySound);
+            CreatePanel();
+            InitImages();
+            rows.Start();            
+        }
+
+        public void ShowBox(int x, int y, int ball)
+        {
+            buttons[x, y].Image = images[ball];
+        }
+        public void PlaySound()
+        {
+            sound = new SoundPlayer("2.wav");
+            sound.Play();
+        }
+        public void Click(Button buttonX)//при нажатии на кнопки
+        {
+            string name = buttonX.Name;
+            int nr = GetNumber(name);
+            int x = nr % Rows.SIZE;
+            int y = nr / Rows.SIZE;
+            Debug.Print($"clicked {name} {x} {y}");
+            rows.Click(x, y);
+        }        
+        public void CreatePanel()
+        {
+            FlowLayoutPanel flowLayoutPanel1 = new FlowLayoutPanel() //игровая сетка
+            {
+                Size = new Size(594, 600),
+                Location = new Point(10, 10),
+                BackColor = Color.Black,                 
+            };
+            Button button_Refresh = new Button() //кнопка обновления поля
+            {
+                Text = "",
+                BackColor = Color.Black,
+                Size = new Size(80, 80),
+                Location = new Point(670, 210),                
+                Image = new Bitmap(Image.FromFile("refresh.png"), new Size(45, 45)),
+            };
+            button_Refresh.Click += (sender, args) => { rows.Start(); };
+
+            Button button_Pause = new Button() //пауза
+            {
+                Text = "",
+                BackColor = Color.Black,
+                Size = new Size(80, 80),
+                Location = new Point(670, 310),
+                Image = new Bitmap(Image.FromFile("pause.png"), new Size(45,45)),
+            };
+            bool paused = false;
+            button_Pause.Click += (sender, args) =>
+            { 
+                if (paused)
+                {
+                    paused = false;
+                    timer1.Start();
+                    flowLayoutPanel1.Enabled = true;
+                    button_Refresh.Enabled = true;
+                    button_Pause.Image = new Bitmap(Image.FromFile("pause.png"), new Size(45, 45));
+                }
+                else
+                {
+                    paused = true;
+                    timer1.Stop();
+                    flowLayoutPanel1.Enabled = false;
+                    button_Refresh.Enabled = false;
+                    button_Pause.Image = new Bitmap(Image.FromFile("play.png"), new Size(45, 45));
+                }
+            };
+
+            Button button_SoundOff = new Button() //мут
+            {
+                Text = "",
+                BackColor = Color.Black,
+                Size = new Size(80, 80),
+                Location = new Point(670, 410),
+                Image = new Bitmap(Image.FromFile("sound-off.png"), new Size(45, 45)),
+            };
+            bool sound_bool = false;
+            button_SoundOff.Click += (sender, args) => 
+            { 
+                if (sound_bool)
+                {
+                    sound_bool = false;
+                    button_SoundOff.Image = new Bitmap(Image.FromFile("sound-off.png"), new Size(45, 45));
+                }
+                else
+                {
+                    sound_bool = true;
+                    button_SoundOff.Image = new Bitmap(Image.FromFile("sound-on.png"), new Size(45, 45));
+                }
+            };
+
+            Button button_Exit = new Button() //выход
+            {
+                Text = "",
+                BackColor = Color.Black,
+                Size = new Size(80, 80),
+                Location = new Point(670, 510),
+                Image = new Bitmap(Image.FromFile("exit.png"), new Size(45, 45)),
+            };
+            button_Exit.Click += (sender, args) => { Application.Exit(); };
+
+            Label label_time = new Label() //время
+            {
+                Text = c,
+                BackColor = Color.Black,
+                Size = new Size(140, 65),
+                Location = new Point(640, 15),
+                Font = new Font(FontFamily.GenericSansSerif, 35),
+                ForeColor = Color.White,
+            };
+
+            Panel panel_GameOver = new Panel() //панель конца игры
+            {
+                BackColor = Color.Black,
+                Size = new Size(450, 320),
+                Location = new Point(180, 120),
+            };
+
+            Label label_score2 = new Label() //счет в конце игры
+            {
+                Text = $"Игра окончена!\r\n    Счет: {rows.score}",
+                BackColor = Color.Transparent,
+                Size = new Size(400, 130),
+                Location = new Point(90, 10),
+                ForeColor = Color.White,
+                Font = new Font(FontFamily.GenericSansSerif, 28),
+            };
+            
+            Label label_score1 = new Label() //счет в процессе игры
+            {
+                Text = $"Текущий счет:\r\n{rows.score}\r\nЛучший счет:\r\n{record}",
+                BackColor = Color.Transparent,
+                Size = new Size(180, 120),
+                Location = new Point(610, 90),
+                ForeColor = Color.White,
+                Font = new Font(FontFamily.GenericSansSerif, 18),
+            };
+
+            Button button_Replay = new Button() //кнопка новой игры
+            {
+                Text = "Играть снова",
+                BackColor = Color.Black,
+                Size = new Size(200, 120),
+                Location = new Point(130, 160),
+                ForeColor = Color.White,
+                Font = new Font(FontFamily.GenericSansSerif, 28),
+            };
+            button_Replay.Click += (sender, args) => { Application.Restart(); };
+
+            Button button_Play = new Button() //кнопка в начале игры
+            {
+                Text = "Играть",
+                BackColor = Color.Black,
+                Size = new Size(300,200),
+                Location = new Point(250,180),
+                Font = new Font(FontFamily.GenericSansSerif, 39),
+                ForeColor = Color.White,
+            };
+            button_Play.Click += (sender, args) =>
+            {
+                flowLayoutPanel1.Visible = true;
+                label_time.Visible = true;
+                button_Pause.Visible = true;
+                button_Play.Visible = true;
+                button_Refresh.Visible = true;
+                button_SoundOff.Visible = true;
+                button_Exit.Visible = true;
+                label_score1.Visible = true;
+                panel_GameOver.Visible = false;
+                button_Play.Visible = false;
+                button_Replay.Visible = false;                
+                label_score2.Visible = false;
+
+                i = 60;
+                c = "00:60";
+                label_time.Text = c;
+                timer1.Interval = 1000;
+                timer1.Enabled = true;
+                timer1.Start();
+            };
+
+            timer1.Tick += (sender, args) =>
+            {
+                tk = --i;
+                TimeSpan span = TimeSpan.FromMinutes(tk);
+                string label = span.ToString("hh':'mm");
+                label_time.Text = label.ToString();
+                label_score1.Text = $"Текущий счет:\r\n{rows.score}\r\nЛучший счет:\r\n{record}";
+                label_score2.Text = $"Игра окончена!\r\n     Счет: {rows.score}";
+                if (i < 11)
+                    label_time.ForeColor = Color.Red;
+                if (i < 0)
+                {
+                    timer1.Stop();              
+                    button_Pause.Image = new Bitmap(Image.FromFile("pause.png"), new Size(45, 45));
+                    flowLayoutPanel1.Enabled = false;
+                    button_Refresh.Enabled = false;
+                    button_Pause.Enabled = false;
+                    panel_GameOver.Visible = true;
+                    label_score2.Visible = true;
+                    button_Replay.Visible = true;
+                    SoundPlayer sound_GameOver = new SoundPlayer("4.wav");
+                    sound_GameOver.Play();
+                    if (isNewRecord())
+                    {
+                        label_score2.Text = $"Игра окончена!\r\nНовый рекорд!\r\n      Счет: {rows.score}";
+                    }
+                }                 
+            };
+
+            panel_GameOver.Controls.Add(label_score2);
+            panel_GameOver.Controls.Add(button_Replay);
+            Controls.Add(button_Exit);
+            Controls.Add(panel_GameOver);
+            Controls.Add(label_score1);
+            Controls.Add(button_Play);
+            Controls.Add(label_time);
+            Controls.Add(button_SoundOff);
+            Controls.Add(button_Pause);
+            Controls.Add(button_Refresh);
+            Controls.Add(flowLayoutPanel1);
+            InitButtons(flowLayoutPanel1);
+
+            button_Replay.Visible = false;
+            label_score2.Visible = false;
+            panel_GameOver.Visible = false;
+            flowLayoutPanel1.Visible = false;
+            button_SoundOff.Visible = false;
+            button_Refresh.Visible = false;
+            button_Pause.Visible = false;
+            button_Exit.Visible = true;
+            label_time.Visible = false;
+            label_score1.Visible = false;
+            button_Play.Visible = true;
+
+        }
+        internal void InitButtons(FlowLayoutPanel flowLayoutPanel1)
+        {
+            buttons = new Button[Rows.SIZE, Rows.SIZE];
+            for (int nr = 0; nr < Rows.SIZE * Rows.SIZE; nr++)
+            {
+                Button button = new Button()
+                {
+                    Name = $"button({nr})",
+                    Text = "",
+                    BackColor = Color.Black,                  
+                    Size = new Size(60, 60),
+                };
+                buttons[nr % Rows.SIZE, nr / Rows.SIZE] = button;
+                button.Click += (sender, args) => { Click( button); };
+                flowLayoutPanel1.Controls.Add(button);
+            }            
+        }        
+        private void InitImages()
+        {
+            images = new Image[Rows.BALLS];
+            for (int j = 0; j < Rows.BALLS; j++)
+            {
+                Bitmap bitmap = new Bitmap(Image.FromFile
+                    (j + filename), new Size(44, 44));
+                images[j] = bitmap;
+            }
+        }
+        public Form1()
+        {
+            InitializeComponent();           
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Start();           
+        }
+        private int GetNumber(string name)
+        {
+            Regex regex = new Regex(@"(\d+)");
+            Match match = regex.Match(name);
+            if (!match.Success)
+            {
+                throw new Exception("Unrecognized object name");
+            }
+            Group group = match.Groups[1];
+            string number = group.Value;
+            return Convert.ToInt32(number);
+        }        
+        private bool isNewRecord()
+        {
+            if (record < rows.score)
+            {
+                File.WriteAllText(Rows.filename, string.Empty);
+                File.WriteAllText(Rows.filename, rows.score.ToString());
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ GameRows/Rows.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Media;
+
+public delegate void ShowBox(int x, int y, int ball);
+public delegate void PlaySound();
+
+namespace GameRows
+{
+    class Rows
+    {
+        public const int SIZE = 9; //размерность массива кнопок
+        public const int BALLS = 7; //кол-во картинок шариков
+        const int ADD_BALLS = 81;
+        public int score = 0;
+        public const string filename = "scores.txt";
+
+        ShowBox showBox;
+        PlaySound playSound;
+        Random random = new Random();
+
+        int[,] map;
+        int fromX, fromY;
+        int toX, toY;
+        bool isBoolTaken;
+        bool canCut = true;
+        internal Rows(ShowBox showBox, PlaySound playSound)
+        {
+            this.showBox = showBox;
+            this.playSound = playSound;
+            map = new int[SIZE, SIZE];
+        }
+        public void Start()
+        {
+            ClearMap();
+            AddAllBalls();
+            CutBalls();
+            isBoolTaken = false;
+            PlayMusic();
+        }
+        public void Click(int x, int y)
+        {
+            if (isBoolTaken)       //если шар взят
+                MoveBall(x, y);    //то его передвинуть
+            else TakeBall(x, y);   //иначе взять
+        }
+        private void TakeBall(int x, int y)
+        {
+            fromX = x;
+            fromY = y;
+            isBoolTaken = true;
+        }
+        private void MoveBall(int x, int y)
+        {
+            toX = x;
+            toY = y;
+            if (!isBoolTaken) return;
+            if (!CanMove(toX, toY)) return;
+            ChangeBalls(fromX, fromY, toX, toY);
+            isBoolTaken = false;
+            if (!CutBalls())
+            {
+                AddRandomBalls();
+                CutBalls();
+                ChangeBalls(toX, toY, fromX, fromY);
+            }
+        }
+        private void ChangeBalls(int x1, int y1, int x2, int y2)
+        {
+            int temp;
+            temp = map[x2, y2];
+            SetMap(x2, y2, map[x1, y1]);
+            SetMap(x1, y1, temp);
+        }
+        private void ClearMap()
+        {
+            for (int x = 0; x < SIZE; x++)
+            {
+                for (int y = 0; y < SIZE; y++)
+                {
+                    SetMap(x, y, 0);  //задает полю пустые картинки (ball==0)
+                }
+            }
+        }
+        private void SetMap(int x, int y, int ball) //задает полю картинки
+        {
+            map[x, y] = ball;                      //задаем
+            showBox(x, y, ball);                   //и показываем
+        }
+        private bool OnMap(int x, int y)
+        {
+            return x >= 0 && x < SIZE &&  //x и y в пределах от 0 до SIZE
+                   y >= 0 && y < SIZE;
+        }
+        private int GetMap(int x, int y) //проверка чтобы не выйти за пределы карты
+        {
+            if (!OnMap(x, y)) return 0; //если есть такая координата
+            return map[x, y];
+        }
+        private void AddRandomBalls()
+        {
+            for (int j = 0; j < ADD_BALLS; j++)
+            {
+                AddRandomBall();
+            }
+        }
+        private void AddRandomBall()
+        {
+            int x, y;
+            int loop = SIZE * SIZE;
+            do
+            {
+                x = random.Next(SIZE);               //присваивать рандомные коррдинаты
+                y = random.Next(SIZE);
+                if (--loop <= 0) return;             //если нет места, то выход
+            } while (map[x, y] > 0);                 //пока не будут заняты все ячейки
+            int ball = 1 + random.Next(BALLS - 1);   //устанавливаем цвет шарика
+            SetMap(x, y, ball);
+        }
+        private void AddAllBalls()
+        {
+            for (int nr = 0; nr < SIZE * SIZE; nr++)
+            {
+                map[nr % SIZE, nr / SIZE] = 1 + random.Next(BALLS - 1);
+                SetMap(nr % SIZE, nr / SIZE, map[nr % SIZE, nr / SIZE]);
+            }
+        }
+        private bool[,] mark; //для хранения координат тех шариков, что нужно удалить
+        private bool CutBalls()
+        {
+            int balls = 0;
+            mark = new bool[SIZE, SIZE];
+            //ищем шарики
+            for (int x = 0; x < SIZE; x++)
+            {
+                for (int y = 0; y < SIZE; y++)
+                {
+                    balls += CutBall(x, y, 1, 0); //вправо
+                    balls += CutBall(x, y, 0, 1); //вниз
+                }
+            }
+            if (balls > 0) //если найденных шариков больше 0
+            {
+                playSound();
+                for (int x = 0; x < SIZE; x++)
+                {
+                    for (int y = 0; y < SIZE; y++)
+                    {
+                        if (mark[x, y]) 
+                        {
+                            SetMap(x, y, 0); //идем по карте и стриаем записанные шарики
+                            score++;
+                        }                        
+                    }
+                }
+                
+                AddRandomBalls();
+                CutBalls();
+                return true;
+            }
+            return false;
+        }
+        private int CutBall(int x0, int y0, int sx, int sy)
+        {
+            int ball = map[x0, y0]; //запоминаем картинку шарика
+            if (ball == 0) return 0;  //если она пустая - выходим
+            int count = 0;
+            for (int x = x0, y = y0; GetMap(x, y) == ball; x += sx, y += sy)
+            {                //движемся вправо и вниз от x0 и y0, не выходим за карту
+                count++; //считаем
+            }
+            if (count < 3) return 0; //нашли меньше трех - не подходит
+            for (int x = x0, y = y0; GetMap(x, y) == ball; x += sx, y += sy)
+            {                //движемся вправо и вниз от x0 и y0, не выходим за карту
+                mark[x, y] = true;  //записываем
+            }
+            return count; //нашли больше трех - выводим количество
+        }
+        private bool CanMove(int toX, int toY)
+        {
+            if (Math.Abs(toX - fromX) == 1 && fromY == toY ||
+                Math.Abs(toY - fromY) == 1 && fromX == toX)
+            {
+                if (canCut) return true;
+                else return false;
+            }
+            else
+            {
+                isBoolTaken = false;
+                return false;
+            }
+        }
+        public void PlayMusic()
+        {
+            //SoundPlayer sound_main = new SoundPlayer("12.wav");
+            //sound_main.PlayLooping();
+        }
+    }
+}
